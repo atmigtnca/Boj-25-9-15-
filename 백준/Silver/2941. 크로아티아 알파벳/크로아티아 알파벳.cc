13f2e2ceb8c7581392b94582ca10ed73cca7e7f1
atmigtnca <goodmorningyb@gmail.com>
2025-09-17 21:08:55 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.cc
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.cc
@@ -4,55 +4,49 @@
 int main(void)
 {
     char ar[101] = {'\0'};
-    int ka[101] = {0};
-    int *pkr = ka;
     scanf("%s", ar);
     int len = strlen(ar);
-    for (int i = 0; i < len; i++)
-    {
-        *(pkr + i) = ar[i];
-    }
     int cut = len; // 총 길이에서 차감
     int ct = 0;    // 증가증가
 
     while (ct < len)
     {
-        if (*(pkr + ct) == 99 && (len - ct >= 2))
+        if (ar[ct] == 'c' && (len - ct >= 2))
         {
-            if (*(pkr + (ct + 1)) == 61 || *(pkr + (ct + 1)) == 45)
+            if (ar[ct + 1] == '=' || ar[ct + 1] == '-')
             {
                 cut--;
                 ct++;
                 continue;
             }
         }
-        else if (*(pkr + ct) == 100 && (len - ct >= 2))
+        else if (ar[ct] == 'd' && (len - ct >= 2))
         {
-            if (*(pkr + (ct + 1)) == 45)
+            if (ar[ct + 1] == '-')
             {
                 cut--;
                 ct += 2;
                 continue;
             }
-            else if (*(pkr + (ct + 1)) == 122 && *(pkr + (ct + 2)) == 61)
+            else if (ar[ct + 1] == 'z' && ar[ct + 2] == '=')
             {
                 cut -= 2;
                 ct += 3;
                 continue;
             }
         }
-        else if ((*(pkr + ct) == 106) && (ct > 0))
+        else if (ar[ct] == 'j' && (ct > 0))
         {
-            if (*(pkr + (ct - 1)) == 108 || *(pkr + (ct - 1)) == 110)
+            if (ar[ct - 1] == 'l' || ar[ct - 1] == 'n')
             {
                 cut--;
                 ct++;
                 continue;
             }
         }
-        else if ((*(pkr + ct) == 61) && (ct > 0))
+        else if (ar[ct] == '=' && (ct > 0))
         {
-            if (*(pkr + (ct - 1)) == 115 || *(pkr + (ct - 1)) == 122)
+            if (ar[ct - 1] == 's' || ar[ct - 1] == 'z')
             {
                 cut--;
                 ct++;
