5b7b50498c6ce7e6483d3395514ceace5f6c49b2
atmigtnca <goodmorningyb@gmail.com>
2025-10-17 14:55:04 +0900

    [Bronze I] Title: ISBN, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/14626. ISBN/ISBN.cc
+++ 백준/Bronze/14626. ISBN/ISBN.cc
@@ -16,38 +16,22 @@ int main()
     auto star = find(isbn.begin(), isbn.end(), '*');
     int idx = distance(isbn.begin(), star), all = 0;
 
-    for (size_t i = 0; i < 12; i++)
+    for (size_t i = 0; i < 13; i++)
     {
         if (i == idx)
         {
             continue;
         }
-        int rt = isbn[i] - '0';
-        if (i % 2 != 0)
-        {
-            all += rt * 3;
-            continue;
-        }
-        all += rt;
+        int rt = isbn[i] - '0', ut = i % 2 ? 3 : 1;
+        all += rt * ut;
     }
-    all += isbn[12] - '0';
 
     int rst = 0;
-    ++idx;
 
-    for (size_t i = 1; i < 10; i++)
+    for (size_t i = 0; i < 10; i++)
     {
-        int I;
-        if (idx % 2 == 0)
-        {
-            I = i * 3;
-        }
-        else
-        {
-            I = i;
-        }
-        int tet = all + I;
-        if (tet % 10 == 0)
+        int ut = idx % 2 ? 3 : 1;
+        if ((all + (i * ut)) % 10 == 0)
         {
             rst = i;
             break;
