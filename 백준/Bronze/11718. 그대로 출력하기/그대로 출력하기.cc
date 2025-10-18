380768235e7966224970ba4e63e7438d5b1073c2
atmigtnca <goodmorningyb@gmail.com>
2025-10-18 23:58:23 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -7,12 +7,11 @@ int main()
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    string hel;
-    hel.reserve(100);
+    string ot;
 
-    while (getline(cin, hel))
+    while (getline(cin, ot))
     {
-        cout << hel << '\n';
+        cout << ot << '\n';
     }
 
     return 0;
