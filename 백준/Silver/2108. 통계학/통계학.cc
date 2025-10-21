5982bdcc604d58431a9fc0b7caa54690b62513e0
atmigtnca <goodmorningyb@gmail.com>
2025-10-21 17:15:50 +0900

    [Silver II] Title: 통계학, Time: 68 ms, Memory: 6116 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.cc
+++ 백준/Silver/2108. 통계학/통계학.cc
@@ -1,59 +1,74 @@
 #include <algorithm>
 #include <cmath>
 #include <iostream>
-#include <map>
 #include <vector>
 using namespace std;
+using ll = long long;
 
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int n, all = 0;
+    ll n, all = 0;
     cin >> n;
-    map<int, int> cut;
-    vector<int> nums(n);
-    vector<int> maxNum;
-    maxNum.reserve(n);
+    vector<ll> nums(n);
+    vector<ll> mod(8001, 0);
 
-    int in;
+    ll in, minIdx = 4000, maxIdx = -4000;
     for (size_t i = 0; i < nums.size(); ++i)
     {
         cin >> in;
         nums[i] = in;
         all += in;
-        ++cut[in];
+        ++mod[in + 4000];
+        if (in < minIdx)
+        {
+            minIdx = in;
+        }
+        if (in > maxIdx)
+        {
+            maxIdx = in;
+        }
+    }
+    minIdx += 4000, maxIdx += 4000;
+
+    vector<ll> modNum;
+    modNum.reserve(maxIdx - minIdx + 1);
+
+    ll max = 0;
+    for (size_t i = minIdx; i <= maxIdx; i++)
+    {
+        if (mod[i] > max)
+        {
+            max = mod[i];
+        }
+    }
+
+    for (size_t i = minIdx; i <= maxIdx; i++)
+    {
+        if (mod[i] == max)
+        {
+            modNum.push_back(i - 4000);
+        }
     }
 
     sort(nums.begin(), nums.end());
 
-    int th, maxIum;
-    auto it1 = max_element(cut.begin(), cut.end(), [](auto& a, auto& b)
-                           { return a.second < b.second; });
+    ll fr, se, th, fo, moMax = 0;
 
-    maxIum = it1->second;
-
-    for (const auto& max : cut)
+    if (modNum.size() >= 2)
     {
-        if (max.second == maxIum)
-        {
-            maxNum.push_back(max.first);
-        }
-    }
-
-    if (maxNum.size() >= 2)
-    {
-        th = maxNum[1];
+        th = modNum[1];
     }
     else
     {
-        th = maxNum[0];
+        th = modNum[0];
     }
 
-    int fr = round((double)all / n);
-    int se = nums[(n / 2)];
-    int fo = nums[n - 1] - nums[0];
+    fr = round((double)all / n);
+    se = nums[(n / 2)];
+    fo = nums[n - 1] - nums[0];
 
     cout << fr << '\n' << se << '\n' << th << '\n' << fo << '\n';
 
