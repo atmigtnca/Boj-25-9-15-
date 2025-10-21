59c9bddecba8f86d8972c4974487d8ed4392a804
atmigtnca <goodmorningyb@gmail.com>
2025-10-22 00:22:24 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+using namespace std;
+using ll = long long;
+#define SU 1234567891
+
+ll rmd(ll a, ll b) { return (a * b) % SU; }
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    ll n;
+    cin >> n;
+
+    string hsg;
+    cin >> hsg;
+
+    ll mtp = 1, rst = 0;
+    for (size_t i = 0; i < n; i++)
+    {
+        ll ap = hsg[i] - 'a' + 1;
+        rst += rmd(ap, mtp);
+        mtp *= 31;
+    }
+
+    rst %= SU;
+
+    cout << rst << '\n';
+
+    return 0;
+}
