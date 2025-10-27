fb46ca3a698a8b73da63d0778e339c4dd437b8bc
atmigtnca <goodmorningyb@gmail.com>
2025-10-27 15:04:42 +0900

    [Silver III] Title: 분수 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1735. 분수 합/분수 합.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+using ll = long long;
+
+int gcd(int a, int b)
+{
+    while (b != 0)
+    {
+        int tmp = b;
+        b = a % b;
+        a = tmp;
+    }
+    return a;
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int a1, a2, b1, b2;
+    cin >> a1 >> a2;
+    cin >> b1 >> b2;
+
+    int mot = a2 * b2, son = (a1 * b2) + (b1 * a2);
+
+    int div = gcd(son, mot);
+    mot /= div, son /= div;
+
+    cout << son << ' ' << mot << '\n';
+    return 0;
+}
