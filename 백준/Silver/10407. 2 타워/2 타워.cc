e74c3dc2dd627f20d83329cabd66e2bdf76b26c3
atmigtnca <goodmorningyb@gmail.com>
2025-11-01 12:02:09 +0900

    [Silver III] Title: 2 타워, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/10407. 2 타워/2 타워.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string h;
+    cin >> h;
+
+    if (h == "1")
+        cout << 2 << '\n';
+    else
+        cout << 1 << '\n';
+
+    return 0;
+}
