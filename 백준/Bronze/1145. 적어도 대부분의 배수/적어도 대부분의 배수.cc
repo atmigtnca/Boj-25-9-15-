4c32685a8c5fa265708ce8bbf142502e9b58e796
atmigtnca <goodmorningyb@gmail.com>
2025-11-10 19:39:35 +0900

    [Bronze I] Title: 적어도 대부분의 배수, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1145. 적어도 대부분의 배수/적어도 대부분의 배수.cc
@@ -0,0 +1,39 @@
+#include <array>
+#include <iostream>
+using namespace std;
+using ll = long long;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    array<int, 5> dou;
+    for (size_t i = 0; i < 5; i++)
+    {
+        cin >> dou[i];
+    }
+
+    int str = 1, rst;
+    while (true)
+    {
+        int cut = 0;
+        for (size_t i = 0; i < 5; i++)
+        {
+            int n = str % dou[i];
+            if (n == 0)
+            {
+                cut++;
+            }
+        }
+        if (cut >= 3)
+        {
+            rst = str;
+            break;
+        }
+        str++;
+    }
+    cout << rst << '\n';
+
+    return 0;
+}
