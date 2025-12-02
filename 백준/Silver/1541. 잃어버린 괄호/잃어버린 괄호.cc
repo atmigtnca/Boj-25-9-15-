6f0c32d00b732a26d282f12b1819e75115e1335c
atmigtnca <goodmorningyb@gmail.com>
2025-12-02 21:49:15 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.cc
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.cc
@@ -1,95 +1,48 @@
-#include <cmath>
 #include <iostream>
 #include <string>
-#include <vector>
 using namespace std;
 using ll = long long;
-vector<int> nums;
-vector<char> plma;
-
-int fid(int cur, string& ipt)
-{
-    int size = 0, tmp = cur;
-    for (int i = cur; i < cur + 5; i++)
-    {
-        if ('0' > ipt[i] || ipt[i] > '9')
-        {
-            break;
-        }
-        size++;
-    }
-
-    int ten = pow(10, size - 1), rst = 0;
-    for (int i = cur; i < cur + size; i++)
-    {
-        int num = ipt[i] - '0';
-        rst += num * ten;
-        ten /= 10;
-    }
-
-    nums.push_back(rst);
-    if (cur + size < ipt.size())
-    {
-        plma.push_back(ipt[cur + size]);
-    }
-    return ++size;
-}
-
-void trans()
-{
-    plma.push_back('+');
-    string ipt;
-    cin >> ipt;
-
-    int cur = 0;
-    while (cur < ipt.size())
-    {
-        int len = fid(cur, ipt);
-        cur += len;
-    }
-}
-
-int cal()
-{
-    int rst = 0;
-    int cur = nums.size();
-
-    for (size_t i = 0; i < nums.size(); i++)
-    {
-        if (plma[i] == '+')
-        {
-            rst += nums[i];
-            // cout << i << ' ' << nums[i] << "ppp\n";
-        }
-        else
-        {
-            for (int j = i; j < nums.size(); j++)
-            {
-                rst -= nums[j];
-                // cout << j << ' ' << nums[j] << "mmm\n";
-                if (plma[j + 1] == '-' || j == nums.size() - 1)
-                {
-                    i = j;
-                    // cout << j + 1 << '\n';
-                    break;
-                }
-            }
-        }
-    }
-
-    return rst;
-}
 
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0), cout.tie(0);
-    nums.reserve(51);
-    plma.reserve(31);
 
-    trans();
+    string ipt;
+    cin >> ipt;
 
-    cout << cal() << '\n';
+    string tmp = "";
+    int rst = 0;
+    bool chk = false;
+
+    for (size_t i = 0; i <= ipt.size(); i++)
+    {
+        if (ipt[i] == '-' || ipt[i] == '+' || i == ipt.size())
+        {
+            int num = stoi(tmp);
+
+            if (chk)
+            {
+                rst -= num;
+            }
+            else
+            {
+                rst += num;
+            }
+
+            if (ipt[i] == '-')
+            {
+                chk = true;
+            }
+            tmp = "";
+        }
+        else
+        {
+            tmp += ipt[i];
+        }
+    }
+
+    cout << rst << '\n';
 
     return 0;
 }
