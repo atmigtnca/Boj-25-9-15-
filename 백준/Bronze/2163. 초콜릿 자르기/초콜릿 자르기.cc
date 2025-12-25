590d752cdd9887b474819f542b26462c5d14b8b7
JongHo Seo <goodmorningyb@gmail.com>
2025-12-25 23:48:04 +0900

    [Bronze I] Title: 초콜릿 자르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2163. 초콜릿 자르기/초콜릿 자르기.cc
+++ 백준/Bronze/2163. 초콜릿 자르기/초콜릿 자르기.cc
@@ -10,23 +10,9 @@ int main()
     int N, M;
 
     cin >> N >> M;
-
-    if (M == 1 && N == 1)
-    {
-        cout << 0 << '\n';
-    }
-    else if (M == 1)
-    {
-        cout << N - 1 << '\n';
-    }
-    else if (N == 1)
-    {
-        cout << M - 1 << '\n';
-    }
-    else
-    {
-        cout << (N - 1) + ((M - 1) * N) << '\n';
-    }
+    
+    cout << M * N - 1 << '\n';
+    
 
     return 0;
 }
