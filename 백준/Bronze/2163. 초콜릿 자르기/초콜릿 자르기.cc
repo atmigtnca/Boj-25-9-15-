72933f32758a7fdf86c35c4a1c445c6e83f538c0
JongHo Seo <goodmorningyb@gmail.com>
2025-12-25 23:49:29 +0900

    [Bronze I] Title: 초콜릿 자르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2163. 초콜릿 자르기/초콜릿 자르기.cc
+++ 백준/Bronze/2163. 초콜릿 자르기/초콜릿 자르기.cc
@@ -1,18 +1,9 @@
 #include <iostream>
-using namespace std;
-using ll = long long;
 
 int main()
 {
-    ios::sync_with_stdio(0);
-    cin.tie(0), cout.tie(0);
-
     int N, M;
-
-    cin >> N >> M;
-    
-    cout << M * N - 1 << '\n';
-    
-
+    std::cin >> N >> M;
+    std::cout << M * N - 1;
     return 0;
 }
