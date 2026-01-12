887fb060d76eafb429cea2f95c482a5b8599bebd
JongHo Seo <goodmorningyb@gmail.com>
2026-01-12 11:00:36 +0900

    [Silver II] Title: 상자넣기, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/1965. 상자넣기/상자넣기.cc
@@ -0,0 +1,46 @@
+#include <algorithm>
+#include <iostream>
+using namespace std;
+using ll = long long;
+int ary[1001];
+int dp[1001];
+int n;
+
+void input()
+{
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> ary[i];
+        dp[i] = 1;
+    }
+}
+
+void rstfnc()
+{
+    for (int i = 1; i <= n; i++)
+    {
+        int cur = ary[i];
+        for (int j = 1; j < i; j++)
+        {
+            if (ary[j] < cur && dp[j] >= dp[i])
+            {
+                dp[i] = dp[j] + 1;
+            }
+        }
+    }
+
+    auto rst = max_element(&dp[1], &dp[n] + 1);
+    cout << *rst << '\n';
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+
+    input();
+    rstfnc();
+
+    return 0;
+}
