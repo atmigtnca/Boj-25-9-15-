bb8869f1d735ddaa72d83a8c1611ecdc865e2f96
JongHo Seo <goodmorningyb@gmail.com>
2026-01-13 10:08:29 +0900

    [Gold III] Title: 나머지 합, Time: 116 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/10986. 나머지 합/나머지 합.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+using ll = long long;
+vector<ll> cnt;
+ll n, m, rst = 0;
+
+void solve()
+{
+    for (auto mem : cnt)
+    {
+        if (!mem)
+        {
+            continue;
+        }
+        rst += (mem * (mem - 1)) / 2;
+    }
+}
+
+void input()
+{
+    cin >> n >> m;
+    cnt.resize(m);
+
+    ll sum = 0;
+
+    for (int i = 1; i <= n; i++)
+    {
+        ll ipt;
+        cin >> ipt;
+        sum = (sum + ipt % m) % m;
+        if (!sum)
+        {
+            rst++;
+        }
+        cnt[sum]++;
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+
+    input();
+    solve();
+    cout << rst << '\n';
+
+    return 0;
+}
